86279448a68779ac6feb32625f787bda05b87e8b
James Grubb <jamesgrubb@mac.com>
2019-05-15 23:37:52 +0100

    Added large 50

--- hash/src/components/Content/Content.js
+++ hash/src/components/Content/Content.js
@@ -1,77 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import airtable from '../../utils/airtable';
-import { ReactComponent as Mask } from '../../logo.svg'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import airtable from "../../utils/airtable";
 
 const Breadcrumb = styled.h3.attrs({
-className: "f7 ttu o-50 dark-green tracked"
-})`
-
-`
+  className: "f7 ttu o-50 dark-green tracked"
+})``;
 const Main = styled.main.attrs({
-className: ""
+  className: ""
 })`
-grid-column: 2 / span 1
-display: grid;
-grid-template-columns: repeat(5, 1fr);
-grid-template-rows: repeat(6, 1fr)
-`
+  z-index: 1;
+  grid-column: 2 / span 1;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  /* grid-template-rows: repeat(6, 1fr); */
+`;
 
 const Content = styled.article.attrs({
-    className: "w-100 vh-100 pa3"
+  className: "w-100 vh-100 pa3"
 })`
-grid-column: 1 / -1
-`
+  grid-column: 1 / -1;
+`;
 const Url = styled.a.attrs({
-    className: "link"
+  className: "link"
 })`
-&::after{
-    content: "→"
-}
-`
+  &::after {
+    content: "→";
+  }
+`;
 
 const MaskWrapper = styled.div`
-grid-column: 3 / -1;
-grid-row: 3 / -1
-`
+  grid-column: 3 / -1;
+  grid-row: 3 / -1;
+`;
 
-const Figure = (props) => {
-    return(
-        <img src={props.url} alt="" />
-    )
-}
+const ContentWrapper = props => {
+  const [state, setState] = useState({
+    records: []
+  });
 
+  useEffect(() => {
+    airtable(setState);
+  }, []);
+  console.log(state.records);
 
-const ContentWrapper = (props) => {
-
-    
-   
-   const [state, setState] = useState({
-        records: []
-    });
-
-    useEffect(() => {
-        airtable(setState)
-    },[])
-    console.log(state.records)
-
-return(
-    
+  return (
     <Main>
-        {state.records.length > 0 ? (state.records.map((record,index)=>{
-           return(
+      {state.records.length > 0 ? (
+        state.records.map((record, index) => {
+          return (
             <Content key={index} id={record.fields["Link"]}>
-                <Breadcrumb>{record.fields["Link"]}</Breadcrumb>
-            <h1>{record.fields["Title"]}</h1>
-            <p className="dark-green lh-copy">{record.fields["Description"]}</p>
-            {record.fields["Url"] ? (<Url href={record.fields["Url"]}>{record.fields["UrlLink"]}</Url>) : null}
-            <MaskWrapper>
-                <Mask />
-            </MaskWrapper>
-            <Figure url={record.fields['Image']} />
-            </Content>)
-        })) : (<p>Loading</p>)}
+              <Breadcrumb>{record.fields["Link"]}</Breadcrumb>
+              <h1>{record.fields["Title"]}</h1>
+              <p className="dark-green lh-copy">
+                {record.fields["Description"]}
+              </p>
+              {record.fields["Url"] ? (
+                <Url href={record.fields["Url"]}>
+                  {record.fields["UrlLink"]}
+                </Url>
+              ) : null}
+            </Content>
+          );
+        })
+      ) : (
+        <p>Loading</p>
+      )}
     </Main>
-)}
+  );
+};
 
-export default ContentWrapper
+export default ContentWrapper;
--- hash/src/routes.js
+++ hash/src/routes.js
@@ -1,31 +1,50 @@
-import React from 'react';
-import styled from 'styled-components'
-import {NavBar} from './components/NavBar'
-import {Route} from 'react-router-dom'
-import {Content} from './components/Content'
-import 'tachyons';
+import React from "react";
+import styled from "styled-components";
+import { NavBar } from "./components/NavBar";
+import { Route } from "react-router-dom";
+import { Content } from "./components/Content";
+import { ReactComponent as Fifty } from "./fifty.svg";
+import "tachyons";
 
 const Container = styled.div.attrs({
-  className : "container w-100 h-100 pa2 bg-light-green mw8"
+  className: "container w-100 h-100 pa2 bg-light-green mw8 relative"
 })`
---grid: minmax(100px, 300px) minmax(15rem, 2fr);
-display: grid;
-grid-template-columns: var(--grid);
-@media all and (max-width:200px){
-  --grid: 1fr
-}
-`
+  --grid: minmax(100px, 300px) minmax(15rem, 2fr);
+  display: grid;
+  grid-template-columns: var(--grid);
+  @media all and (max-width: 200px) {
+    --grid: 1fr;
+  }
+`;
+const ContainerBackground = styled.div.attrs({
+  className: "fixed vh-100"
+})`
+  z-index: 0;
+  display: grid;
+  grid-template-rows: repeat(6, 1fr);
+  grid-column: 1 / span 1;
+  > svg {
+    fill: #e8fdf5;
+    width: auto;
+    height: 100%;
+    grid-row: 1 / span 6;
+    transform: translate(-20vmin, -1vmin) scale(1.1);
+  }
+`;
 
 export const Routes = () => {
-  return (    
+  return (
     <Container>
+      <ContainerBackground>
+        <Fifty />
+      </ContainerBackground>
       <NavBar />
       <Content />
       <Route path="/#What" />
       <Route path="/#Where" />
       <Route path="/#When" />
-    </Container>    
+    </Container>
   );
-}
+};
 
 export default Routes;
